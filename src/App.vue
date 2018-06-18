e2ebe2c4c8016461699bc39ddf0b3d735560baa9
EugeneYilia <liuyichen@eugeneliu.xyz>
2018-06-19 05:28:55 +0800

    add navigator

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,27 @@
   <div id="app">
     <img src="./assets/logo.png">
     <router-view/>
+    <Navigator></Navigator>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  import Navigator from "./components/Navigator";
+
+  export default {
+    name: 'App',
+    components: {Navigator}
+
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
--- src/components/Navigator.vue
+++ src/components/Navigator.vue
@@ -1,15 +1,104 @@
 <template>
-    <div>
-
+  <div class="navigator">
+    <div class="item">
+      <img :src="chatSrc" @click="chatClick"/>
+      <!--
+      <img src="../assets/images/DiscussionPressed.png"/>
+      -->
+      <div>
+        聊天
+      </div>
     </div>
+
+    <div class="item">
+      <img v-bind:src="friendsSrc" @click="friendsClick"/>
+      <!--
+      <img src="../assets/images/ResultUnpressed.png"/>
+      -->
+      <div>
+        联系人
+      </div>
+    </div>
+
+    <div class="item">
+      <img v-bind:src="discoverSrc" @click="discoverClick"/>
+      <!--
+      <img src="../assets/images/AnalysisUnpressed.png"/>
+      -->
+      <div>
+        发现
+      </div>
+    </div>
+
+    <div class="item">
+      <img v-bind:src="meSrc" @click="meClick"/>
+      <!--
+      <img src="../assets/images/SelfUnpressed.png"/>
+      -->
+      <div>
+        我
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Navigator.vue"
+  export default {
+    name: "Navigator",
+    data: function () {
+      return {
+        chatSrc: "static/images/DiscussionPressed.png",
+        friendsSrc: "static/images/ResultUnpressed.png",
+        discoverSrc: "static/images/AnalysisUnpressed.png",
+        meSrc: "static/images/SelfUnpressed.png"
+      }
+    },
+    methods: {
+      chatClick: function () {
+          this.chatSrc = "static/images/DiscussionPressed.png",
+          this.friendsSrc = "static/images/ResultUnpressed.png",
+          this.discoverSrc = "static/images/AnalysisUnpressed.png",
+          this.meSrc = "static/images/SelfUnpressed.png"
+      },
+      friendsClick: function () {
+          this.chatSrc = "static/images/DiscussionUnpressed.png",
+          this.friendsSrc = "static/images/ResultPressed.png",
+          this.discoverSrc = "static/images/AnalysisUnpressed.png",
+          this.meSrc = "static/images/SelfUnpressed.png"
+      },
+      discoverClick: function () {
+          this.chatSrc = "static/images/DiscussionUnpressed.png",
+          this.friendsSrc = "static/images/ResultUnpressed.png",
+          this.discoverSrc = "static/images/AnalysisPressed.png",
+          this.meSrc = "static/images/SelfUnpressed.png"
+      },
+      meClick: function () {
+          this.chatSrc = "static/images/DiscussionUnpressed.png",
+          this.friendsSrc = "static/images/ResultUnpressed.png",
+          this.discoverSrc = "static/images/AnalysisUnpressed.png",
+          this.meSrc = "static/images/SelfPressed.png"
+      }
     }
+  }
 </script>
 
 <style scoped>
+  .navigator {
+    /*
+    display: flex;
+    justify-content: center;
+    flex-direction: row;
+    margin: 3px 50px;
+    */
+  }
 
+  .navigator .item {
+    display: inline-block;
+    margin: 3px 5%;
+  }
+
+  .navigator img {
+    width: 30px;
+    height: 30px;
+  }
 </style>
